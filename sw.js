4c793799cfadd58b3481c45b1bc2e620bdf381cf
matthieuayral <matthieu.ayral@gmail.com>
2026-09-11 16:05:32 +0200

    Refresh PWA cache with PNG icons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="carburant-v3";
-self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./app.js","./config.js","./manifest.webmanifest","./icon.svg"]))) });
+const C="carburant-v4";
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./app.js","./config.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"]))) });
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))) });
