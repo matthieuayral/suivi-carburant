f49f51106fad8effc4badde90b23e28315426abc
matthieuayral <matthieu.ayral@gmail.com>
2026-09-11 12:04:19 +0200

    Fix HELXINE tenant ID from Entra

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   clientId: "41cd96b6-5732-4668-86d1-922483e12db6",
-  tenantId: "49ca0281-c4ec-4233-83ad-7380cbb662d7",
+  tenantId: "49ca0281-c4ce-4233-83ad-7380cbb662d7",
   redirectUri: window.location.origin + window.location.pathname,
   workbookPath: "/Suivi_Carburant_V1_2_CORRIGE.xlsx",
   tableName: "TablePleins",
