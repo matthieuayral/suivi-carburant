9fe24c06b93152cc71fbbe0519ac895a0608cb51
matthieuayral <matthieu.ayral@gmail.com>
2026-09-11 12:14:18 +0200

    Bump PWA cache after authentication fix

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-const C="carburant-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./app.js","./config.js","./manifest.webmanifest"]))));self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
+const C="carburant-v2";
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./app.js","./config.js","./manifest.webmanifest"]))) });
+self.addEventListener("activate",e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))),self.clients.claim()])));
+self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))) });
